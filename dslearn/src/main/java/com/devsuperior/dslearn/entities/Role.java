8a23f7f13ced91b0630588265f6d041ea5401476
Carlos Henrique <ichfs9108@gmail.com>
2024-03-07 00:59:49 -0300

    User, Role

---
+++ dslearn/src/main/java/com/devsuperior/dslearn/entities/Role.java
@@ -0,0 +1,55 @@
+package com.devsuperior.dslearn.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    public Role() {
+
+    }
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+
+        Role role = (Role) object;
+
+        return id.equals(role.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
