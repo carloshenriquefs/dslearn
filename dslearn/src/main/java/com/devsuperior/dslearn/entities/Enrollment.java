6564e5721e6170fed383df24d526b77bc2d89cfb
Carlos Henrique <ichfs9108@gmail.com>
2024-03-13 23:11:07 -0300

    Lesson, Content, Task

--- dslearn/src/main/java/com/devsuperior/dslearn/entities/Enrollment.java
+++ dslearn/src/main/java/com/devsuperior/dslearn/entities/Enrollment.java
@@ -16,12 +16,12 @@ public class Enrollment implements Serializable {
     @EmbeddedId
     private EnrollmentPK id = new EnrollmentPK();
 
-    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant enrollMoment;
 
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant refundMoment;
     private boolean available;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private boolean onlyUpdate;
 
     public Enrollment() {
